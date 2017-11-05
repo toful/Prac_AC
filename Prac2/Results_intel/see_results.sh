ba89c3ccaae7fd362fd76b9bd13659972c49d2d0
Aleix Mariné <aleix.marine@estudiants.urv.cat>
2017-11-05 22:45:22 +0100

    recopilades dades practica 2 fase 1

--- Prac2/Results_intel/see_results.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Visualització de les IPC i CPI
-rm Results.txt
-egrep '^sim_IPC' *.txt > Results.txt
-egrep '^bpred_.*_dir_rate' *.txt >> Results.txt
-
--- Prac2/execute.sh
+++ Prac2/execute.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-simplesim-3.0_acx/sim-outorder -config intel_config -redir:sim Results_intel/applu.txt Benchmarks/applu/exe/applu.exe < Benchmarks/applu/data/ref/applu.in > Prova/applu.out 2> Prova/applu.err 
-simplesim-3.0_acx/sim-outorder -config intel_config -redir:sim Results_intel/crafty.txt Benchmarks/crafty/exe/crafty.exe < Benchmarks/crafty/data/ref/crafty.in > Prova/crafty.out 2> Prova/crafty.err
-simplesim-3.0_acx/sim-outorder -config intel_config -redir:sim Results_intel/twolf.txt Benchmarks/twolf/exe/twolf.exe Benchmarks/twolf/data/ref/ref > Prova/ref.stdout 2> Prova/ref.err
+parameters=("-bpred nottaken" "-bpred taken" "-bpred perfect" "-bpred bimod -bpred:bimod 16" "-bpred bimod -bpred:bimod 64" "-bpred bimod -bpred:bimod 256" "-bpred bimod -bpred:bimod 1024" "-bpred bimod -bpred:bimod 4096" "-bpred 2lev -bpred:2lev 1 16 4 1" "-bpred 2lev -bpred:2lev 1 64 6 1" "-bpred 2lev -bpred:2lev 1 256 8 1" "-bpred 2lev -bpred:2lev 1 1024 10 1" "-bpred 2lev -bpred:2lev 1 4096 12 1" "-bpred 2lev -bpred:2lev 1 16 4 0" "-bpred 2lev -bpred:2lev 1 64 6 0" "-bpred 2lev -bpred:2lev 1 256 8 0" "-bpred 2lev -bpred:2lev 1 1024 10 0" "-bpred 2lev -bpred:2lev 1 4096 12 0" "-bpred 2lev -bpred:2lev 8 8 3 0" "-bpred 2lev -bpred:2lev 16 32 5 0" "-bpred 2lev -bpred:2lev 32 128 7 0" "-bpred 2lev -bpred:2lev 64 512 8 0" "-bpred 2lev -bpred:2lev 128 2048 11 0" "-bpred 2lev -bpred:2lev 64 4096 12 0");
+
+
+
+mkdir Results 2> nul
+mkdir Out 2> nul
+echo "" > _Resultats
+j=0
+for i in ${parameters[@]}; do
+
+	simplesim-3.0_acx/sim-outorder -fastfwd 50000000 -max:inst 10000000 ${parameters[$j]} -redir:sim Results/applu.txt Benchmarks/applu/exe/applu.exe < Benchmarks/applu/data/ref/applu.in >> Out/applu.out 2>> Out/applu.err 
+
+	simplesim-3.0_acx/sim-outorder -fastfwd 50000000 -max:inst 10000000 ${parameters[$j]} -redir:sim Results/crafty.txt Benchmarks/crafty/exe/crafty.exe < Benchmarks/crafty/data/ref/crafty.in >> Out/crafty.out 2>> Out/crafty.err
+
+	simplesim-3.0_acx/sim-outorder -fastfwd 50000000 -max:inst 10000000 ${parameters[$j]} -redir:sim Results/twolf.txt Benchmarks/twolf/exe/twolf.exe Benchmarks/twolf/data/ref/ref >> Out/twolf.out 2>> Out/twolf.err
+
+	#simplesim-3.0_acx/sim-outorder -fastfwd 50000000 -max:inst 10000000 ${parameters[$j]} -redir:sim Results/vortex.txt Benchmarks/vortex/exe/vortex.exe lendian1.raw >> Out/vortex.out 2>> Out/vortex.err  
+
 cd Benchmarks/vortex/data/ref/
-../../../../simplesim-3.0_acx/sim-outorder -config ../../../../intel_config -redir:sim ../../../../Results_intel/vortex.txt ../../exe/vortex.exe lendian1.raw > vortex1.out2 2> vortex1.err  
-#simplesim-3.0_acx/sim-outorder -config intel_config -redir:sim Results_intel/vortex1.txt Benchmarks/vortex/exe/vortex.exe Benchmarks/vortex/data/ref/lendian1.raw > Prova/vortex1.out2 2> Prova/vortex1.err
-#simplesim-3.0_acx/sim-outorder -config intel_config Benchmarks/vortex/exe/vortex.exe Benchmarks/vortex/data/ref/lendian2.raw >& Results_intel/vortex2.txt
-#simplesim-3.0_acx/sim-outorder -config intel_config Benchmarks/vortex/exe/vortex.exe Benchmarks/vortex/data/ref/lendian3.raw >& Results_intel/vortex3.txt
+../../../../simplesim-3.0_acx/sim-outorder -fastfwd 50000000 -max:inst 10000000 ${parameters[$j]} -redir:sim ../../../../Results/vortex.txt ../../exe/vortex.exe lendian1.raw > vortex.out 2> vortex.err  
 cd ../../../../
-simplesim-3.0_acx/sim-outorder -config intel_config -redir:sim Results_intel/vpr.txt Benchmarks/vpr/exe/vpr.exe Benchmarks/vpr/data/ref/net.in Benchmarks/vpr/data/ref/arch.in Benchmarks/vpr/data/ref/route.out -nodisp -route_only -route_chan_width 15 -pres_fac_mult 2 -acc_fac 1 -first_iter_pres_fac 4 -initial_pres_fac 8 > Prova/route_log.out 2> Prova/route_log.err
+
+	simplesim-3.0_acx/sim-outorder -fastfwd 50000000 -max:inst 10000000 ${parameters[$j]} -redir:sim Results/vpr.txt Benchmarks/vpr/exe/vpr.exe Benchmarks/vpr/data/ref/net.in Benchmarks/vpr/data/ref/arch.in Benchmarks/vpr/data/ref/route.out -nodisp -route_only -route_chan_width 15 -pres_fac_mult 2 -acc_fac 1 -first_iter_pres_fac 4 -initial_pres_fac 8 >> Out/vpr.out 2>> Out/vpr.err
+
+	echo $j
+	echo ${parameters[$j]}
+	echo \n\n >> _Resultats
+	echo ${parameters[$j]} >> _Resultats
+	echo \n\n >> _Resultats
+
+	more Results/* | grep -E 'IPC|bpred.*_dir_rate|.*.txt$' | tr -s ' ' | cut -d ' ' -f1,2 >> _Resultats
+	let j=j+1
+	if [ $j == 24 ]; then 
+		break 
+	fi
+done
+echo "Final de fase 1"
+rm costs.out
+rm game.001
+rm nul
+exit 0
